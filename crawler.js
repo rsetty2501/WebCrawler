fd6e2db6d542b5f1e930b0f66b41ecd5bae8f683
rsetty2501 <rahulssetty@gmail.com>
2020-03-14 19:47:24 +0100

    Initial commit

---
+++ crawler.js
@@ -0,0 +1,8 @@
+/* 
+    Web Crawler using JavaScript and Node.js
+    - Node.js is an open-source JavaScript runtime environment that executes JS code outside of a browser
+    - Libraries used for web crawler:
+        - HTTP
+        - Cheerio
+        - url-parse
+*/
